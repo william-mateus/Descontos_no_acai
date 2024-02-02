1a3fbcddb9ea90a48a6a6e0b061998dd7c729f39
william-mateus <william.mtt003@gmail.com>
2024-02-02 19:58:08 -0300

    foi adicionado uma custom Snackbar

--- app/src/main/java/br/com/willaimsilva/appdanet/MainActivity.kt
+++ app/src/main/java/br/com/willaimsilva/appdanet/MainActivity.kt
@@ -1,12 +1,12 @@
 package br.com.willaimsilva.appdanet
 
-import android.app.AlertDialog
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-import android.view.View
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import br.com.willaimsilva.appdanet.databinding.ActivityMainBinding
+import br.com.willaimsilva.appdanet.databinding.CustomSnackbarBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
@@ -16,54 +16,61 @@ class MainActivity : AppCompatActivity() {
     private fun validarCampo(piranha: String?): Boolean {
         return piranha.isNullOrEmpty() || piranha.toFloatOrNull() == null
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnDesconto.setOnClickListener {
-
-            if (validarCampo(binding.desconto.text.toString())){
-                mensagem(
-                    view =it,
-                    menssage = "Preencha TODOS os campos !!",
-                    context = this,
-                    color =R.color.red
+            if (validarCampo(binding.desconto.text.toString())) {
+                mostrarMensagem(
+                    mensagem = "Preencha TODOS os campos !!",
+                    color = R.color.red,
+                    imageDrawable = R.drawable.erro
                 )
-            }
-
-            else{
+            } else {
                 val desconto = binding.desconto.text.toString().toDouble();
                 val preco = binding.preco.text.toString().toDouble();
 
-                val formula = preco*(desconto/100);
+                val formula = preco * (desconto / 100);
                 val valorFinal = preco - formula;
 
-                mensagem(
-                    view =it,
-                    menssage = "Este é o novo preço R$: $valorFinal reais",
-                    context = this,
-                    color =R.color.purple_700
+                mostrarMensagem(
+                    mensagem = "Este é o novo preço R$: $valorFinal reais",
+                    color = R.color.snackbar,
+                    imageDrawable = R.drawable.icon
                 )
-                if (valorFinal == 0.0){
-                    mensagem(
-                        view =it,
-                        menssage = "voce descobriu o Easter egg, parabnes !!",
-                        context = this,
-                        color =R.color.teal_200
+                if (valorFinal == 0.0) {
+                    mostrarMensagem(
+                        mensagem = "voce descobriu o Easter egg, parabnes !!",
+                        color = R.color.teal_200,
+                        imageDrawable = R.drawable.icon_easteegg
                     )
                 }
             }
         }
     }
-    fun mensagem (
-        view: View,
-        menssage: String,
-        context: Context,
-        color:Int
+
+    fun mostrarMensagem(
+        mensagem: String,
+        color: Int,
+        imageDrawable: Int
     ) {
-        val snackbar = Snackbar.make(this,view,menssage, Snackbar.LENGTH_SHORT)
-        snackbar.view.setBackgroundColor(ContextCompat.getColor(context,color))
+        val view = CustomSnackbarBinding.inflate(LayoutInflater.from(this))
+        view.textSnackbar.text = mensagem
+        view.root.setCardBackgroundColor(ContextCompat.getColor(this, color))
+
+        //trocar imagem snackbar
+        view.profilrPic.setImageResource(imageDrawable);
+
+        // CoordinatorLayout
+
+        val snackbar = Snackbar.make(this, binding.root, "", Snackbar.LENGTH_SHORT)
+        val snackbarView = snackbar.view as? Snackbar.SnackbarLayout;
+        snackbarView?.setPadding(0, 0, 0, 0);
+        snackbarView?.addView(view.root)
+        snackbarView?.setBackgroundColor(Color.TRANSPARENT)
         snackbar.show()
     }
 }
